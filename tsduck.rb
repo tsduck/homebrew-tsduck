0a873167c0b85e774319e95555a8069f6fd8a779
Thierry Lelegard <thierry@lelegard.fr>
2017-09-24 15:07:40 +0200

    Added script to automatically update formula to latest release

--- tsduck.rb
+++ tsduck.rb
@@ -3,9 +3,9 @@ class Tsduck < Formula
   desc "MPEG Transport Stream Toolkit"
   homepage "https://tsduck.github.io/"
 
-  version "3.3-20170915"
-  url "https://github.com/tsduck/tsduck/archive/v3.3-20170915.tar.gz"
-  sha256 "b2fe39e3392833d8ffe1e9529b5c09086aa8ec0b2e207fc635434022e3949ee1"
+  version "3.3-20170924"
+  url "https://github.com/tsduck/tsduck/archive/v3.3-20170924.tar.gz"
+  sha256 "8d8a336cbc03ba2480d3c480e9e7dfaea47b9f05bb949ab7a7451491a5e74156"
 
   depends_on "pcsc-lite"
   depends_on "cppunit" => :build
@@ -13,7 +13,7 @@ class Tsduck < Formula
   depends_on "grep" => :build
 
   def install
-    system "make"
+    system "make", "NOTEST=true"
     system "make", "install", "install-devel", "SYSPREFIX=#{prefix}"
   end
 
---
+++ update-formula.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# This script automatically updates the tsduck formula for the most recent version.
+#
+
+SCRIPT=$(basename $0 .sh)
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+
+info() { echo >&2 "$SCRIPT: $*"; }
+error() { echo >&2 "$SCRIPT: $*"; exit 1; }
+
+# Get the formula file.
+FORMULA="$SCRIPTDIR/tsduck.rb"
+[[ -s "$FORMULA" ]] || error "formula file not found: $FORMULA"
+
+# Get the tag of the most recent release in the tsduck repository.
+TAG=$(curl -s https://api.github.com/repos/tsduck/tsduck/releases/latest |
+          grep '"tag_name":' |
+          head -1 |
+          sed -e 's/.*: *"//' -e 's/".*//')
+[[ -n "$TAG" ]] || error "no tag found for latest tsduck release"
+[[ $TAG = v* ]] || errro "suspect tag '$TAG', should be 'vX.Y-YYYYMMDD'"
+VERSION=${TAG/v/}
+info "version: $VERSION"
+
+# URL of the source tarball.
+URL="https://github.com/tsduck/tsduck/archive/${TAG}.tar.gz"
+info "tarball: $URL"
+
+# Get checksum of source tarball.
+SHA256=$(curl -L -s "$URL" | shasum -a 256 | awk '{print $1}')
+info "sha256: $SHA256"
+
+# Update formula for latest release.
+gsed -i \
+    -e "s|version *\".*\"|version \"$VERSION\"|" \
+    -e "s|url *\".*\"|url \"$URL\"|" \
+    -e "s|sha256 *\".*\"|sha256 \"$SHA256\"|" \
+    "$FORMULA"
