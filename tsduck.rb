93e3cbaadef02f3024783c0a80a03165da04351b
Thierry Lelegard <thierry@lelegard.fr>
2019-11-22 23:46:30 +0100

    Version 3.19-1520

--- tsduck.rb
+++ tsduck.rb
@@ -3,9 +3,9 @@ class Tsduck < Formula
   desc "MPEG Transport Stream Toolkit"
   homepage "https://tsduck.github.io/"
 
-  version "3.18-1361"
-  url "https://github.com/tsduck/tsduck/archive/v3.18-1361.tar.gz"
-  sha256 "619e8a201d417e1f559b02e67c15a104bf4c030326a9401371bc56942d5f476a"
+  version "3.19-1520"
+  url "https://github.com/tsduck/tsduck/archive/v3.19-1520.tar.gz"
+  sha256 "f7274f6c4b19bf43d6de07de5fc1bd13cb3eea107f602db5d74271dffea391e3"
 
   depends_on "pcsc-lite"
   depends_on "cppunit" => :build
