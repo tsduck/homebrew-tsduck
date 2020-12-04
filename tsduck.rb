60abe80744e9ebf769dc03bf5cceeb9b48a84e1b
Thierry Lelegard <thierry@lelegard.fr>
2020-12-04 23:31:26 +0100

    Version 3.24-2109

--- tsduck.rb
+++ tsduck.rb
@@ -3,9 +3,9 @@ class Tsduck < Formula
   desc "MPEG Transport Stream Toolkit"
   homepage "https://tsduck.github.io/"
 
-  version "3.23-1965"
-  url "https://github.com/tsduck/tsduck/archive/v3.23-1965.tar.gz"
-  sha256 "b0b717714ee5b5d9c554169501b002ab34bec7586ae9561fe2728244a39b886c"
+  version "3.24-2109"
+  url "https://github.com/tsduck/tsduck/archive/v3.24-2109.tar.gz"
+  sha256 "7d2ca53be65d1de4248f906db9cd10fa5edc9148d2c5840a0398de9358942c5d"
 
   depends_on "pcsc-lite"
   depends_on "gnu-sed" => :build
