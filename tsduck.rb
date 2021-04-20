bc991afb8a69058d121d685d219fb17091f54010
Thierry Lelegard <thierry@lelegard.fr>
2021-04-20 23:33:21 +0200

    Version 3.26-2349

--- tsduck.rb
+++ tsduck.rb
@@ -3,9 +3,9 @@ class Tsduck < Formula
   desc "MPEG Transport Stream Toolkit"
   homepage "https://tsduck.io/"
 
-  version "3.25-2237"
-  url "https://github.com/tsduck/tsduck/archive/v3.25-2237.tar.gz"
-  sha256 "f2c1c86da32b625ef723c0c33090718926767d44db406e655a95322934eaa195"
+  version "3.26-2349"
+  url "https://github.com/tsduck/tsduck/archive/v3.26-2349.tar.gz"
+  sha256 "79fc421ca1c938badc3d5f236ab739c3540057d1084d445da7c955eb2e99087d"
   
   head "https://github.com/tsduck/tsduck.git"
 
